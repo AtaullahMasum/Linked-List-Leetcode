c6bec79f2e8c3fb0d770144dd2a9d699adc52d2f
AtaullahMasum <34019692+AtaullahMasum@users.noreply.github.com>
2024-03-26 03:50:47 +0600

    optimal Solution added

--- Given a linked list of 0s, 1s and 2s, sort it.py
+++ Given a linked list of 0s, 1s and 2s, sort it.py
@@ -42,4 +42,34 @@ class Solution:
                 count2 -= 1
             temp = temp.next
         return head
+#Optimal Approch
+class Solution:
+    #Function to sort a linked list of 0s, 1s and 2s.
+    def segregate(self, head):
+        #code here
+        Zerohead = Node(-1)
+        Onehead = Node(-1)
+        Twohead = Node(-1)
+        zero = Zerohead
+        one = Onehead
+        two = Twohead
+        temp = head
+        while temp:
+            if temp.data == 0:
+                zero.next = temp
+                zero = zero.next
+            elif temp.data == 1:
+                one.next = temp
+                one = one.next
+            else:
+                two.next = temp
+                two = two.next
+            temp = temp.next
+        if one.data != -1:
+            zero.next = Onehead.next
+        else:
+            zero.next = Twohead.next
+        one.next = Twohead.next
+        two.next = None
+        return Zerohead.next
     
