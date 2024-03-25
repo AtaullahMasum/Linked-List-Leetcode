b834a22eab61d2d20499c0e8c2a9415c66947295
AtaullahMasum <34019692+AtaullahMasum@users.noreply.github.com>
2024-03-26 04:59:51 +0600

    comment added

--- 2095. Delete the Middle Node of a Linked List.py
+++ 2095. Delete the Middle Node of a Linked List.py
@@ -10,10 +10,12 @@ class Solution:
         slow = head 
         fast = head
         prev = None
+        # Move slow pointer one step at a time and fast pointer two steps at a time
         while fast and fast.next:
             prev = slow
             slow = slow.next
             fast = fast.next.next
+        # Delete the middle node by updating the next pointer of the node before it
         prev.next = slow.next
         return head
         
