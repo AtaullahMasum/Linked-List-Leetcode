dc9d8a146bd692c3b2b33c781139c9cbf02fe9fe
AtaullahMasum <34019692+AtaullahMasum@users.noreply.github.com>
2024-03-26 20:14:33 +0600

    better approches

--- 141. Linked List Cycle.py
+++ 141. Linked List Cycle.py
@@ -15,3 +15,28 @@ class Solution:
             if slow == fast:
                 return True
         return False
+# Better Approch
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+       # Check if the linked list is empty or has only one node
+        if not head or not head.next:
+            return None
+        
+        slow = head
+        fast = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
+                slow = head
+                while slow != fast:
+                    slow = slow.next
+                    fast = fast.next
+                return slow
+        return None
